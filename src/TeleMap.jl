4228d98f453c256d45d8bfc1ee0a5a673a24a326
Nikola Pocuca <nikpocuca@gmail.com>
2019-07-28 12:54:07 -0400

    forgot to export speedtypes

--- src/TeleMap.jl
+++ src/TeleMap.jl
@@ -7,7 +7,7 @@ module TeleMap
 
 	import Dates, KernelDensity, Rmath, DataFrames, Query, RecipesBase
 
-	export KinematicEvent, KinematicEvents, KinematicMap, DeviationEvents
+	export KinematicEvent, KinematicEvents, KinematicMap, DeviationEvents. MPH, KMH
 
 	include("Kinematic.jl")
 

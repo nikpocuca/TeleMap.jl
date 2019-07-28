099323a2ecaace1ad727ec557d403f8f00fd8a60
Nikola Pocuca <nikpocuca@gmail.com>
2019-07-28 00:27:36 -0400

    fixed outlier

--- src/Kinematic.jl
+++ src/Kinematic.jl
@@ -1,10 +1,7 @@
 # Kinematic julia code for the purposes of analyzing telemtric data pertaining to speed,
 # acceleration and other measurements dealing of the kinematic nature.
 
-using Dates, KernelDensity, Plots
-
-
-abstract type SpeedType end;
+using Dates, KernelDensity, Plots, Rmath, Roots, DataFrames, Query
 
 struct KMH <: SpeedType end;
 struct MPH <: SpeedType end;
@@ -111,6 +108,21 @@ mutable struct KinematicEvents<:TelemetricEvents
 end
 
 
+function plot(ev::KinematicEvents)
+    vels::Array{Float64,1} = ev.vels;
+    aecs::Array{Float64,1} = ev.aecs;
+    scatter(vels,aecs);
+end
+
+
+function plot!(ev::KinematicEvents)
+    vels::Array{Float64,1} = ev.vels;
+    aecs::Array{Float64,1} = ev.aecs;
+    scatter!(vels,aecs);
+end
+
+
+
 
 """
 Kinematic Map Structure
@@ -128,6 +140,7 @@ mutable struct KinematicMap <: TelemetricMap
     vel_bounds::Array{Float64,1}
     aec_bounds::Array{Float64,1}
     kernel::BivariateKDE
+    events::KinematicEvents
     function KinematicMap(kes::KinematicEvents,vel_bounds::Array{Float64,1}, aec_bounds::Array{Float64,1})
         # Check errors
         if(size(vel_bounds,1) != 2)
@@ -139,13 +152,25 @@ mutable struct KinematicMap <: TelemetricMap
         try
             vels::Array{Float64,1} = kes.vels;
             aecs::Array{Float64,1} = kes.aecs;
+            dat::DataFrame = DataFrame(vels = vels, aecs = aecs, tstamps = kes.tstamps);
+            dat_filtered::DataFrame = @from i in dat begin
+                            @where i.vels > vel_bounds[1] && i.vels < vel_bounds[2]
+                            @where i.aecs > aec_bounds[1] && i.aecs < aec_bounds[2]
+                            @select {i.vels , i.aecs, i.tstamps}
+                            @collect DataFrame
+                        end
 
-            checks = (vels .> vel_bounds[1]) .& (vels .< vel_bounds[2]) .& (aecs .> aec_bounds[1]) .& (aecs .< aec_bounds[2])
-            vels_filtered::Array{Float64,1} = vels[checks];
-            aecs_filtered::Array{Float64,1} = aecs[checks];
+            vels_filtered::Array{Float64,1} = convert(Array{Float64,1},dat_filtered.vels);
+            aecs_filtered::Array{Float64,1} = convert(Array{Float64,1},dat_filtered.aecs);
 
             kernel::BivariateKDE = kde((vels_filtered,aecs_filtered)) # BivariateKDE
-            km::KinematicMap = new(vel_bounds,aec_bounds,kernel);
+
+            events = KinematicEvents(vels_filtered,
+                                    aecs_filtered,
+                                    kes.speedtype,
+                                    convert(Array{DateTime,1},dat_filtered.tstamps));
+
+            km::KinematicMap = new(vel_bounds,aec_bounds,kernel,events);
             return (km);
         catch
             error("Kernel KernelDensity estimation failed, try using debugger and entering the function")
@@ -178,7 +203,18 @@ function plot(km::KinematicMap)
                 km.aec_bounds[1]:y_h:km.aec_bounds[2])
     # Interpolate
     ik = InterpKDE(km.kernel);
-    contour(xy_grid[1],xy_grid[2],pdf(ik,xy_grid[1],xy_grid[2]))
+    heatmap(xy_grid[1],xy_grid[2],pdf(ik,xy_grid[1],xy_grid[2])',fill = true,color = :plasma)
+end
+
+
+function plot!(km::KinematicMap)
+    x_h::Float64 = (km.vel_bounds[2] - km.vel_bounds[1])/100
+    y_h::Float64 = (km.aec_bounds[2] - km.aec_bounds[1])/100
+    xy_grid = (km.vel_bounds[1]:x_h:km.vel_bounds[2],
+                km.aec_bounds[1]:y_h:km.aec_bounds[2])
+    # Interpolate
+    ik = InterpKDE(km.kernel);
+    contour!(xy_grid[1],xy_grid[2],pdf(ik,xy_grid[1],xy_grid[2]))
 end
 
 
@@ -198,22 +234,112 @@ p_kernel, UnivariateKDE used to calculate
 mutable struct DeviationEvents <: TelemetricEvents
     α::Float64 # power level alpha, default is usually 0.05
     events::KinematicEvents
-    map::KinematicMap
+    bikernel::BivariateKDE
     deviants::KinematicEvents
     p_kernel::UnivariateKDE
-    function DeviationEvents(events,map,α = 0.05)
-        p_is::Array{Float64,1} = map( (v,a) -> pdf(map.kernel,v,a), events.vels,events.aecs)
+    p_is::Array{Float64,1}
+    p_star::Float64
+    function DeviationEvents(events::KinematicEvents,bikernel::BivariateKDE,α = 0.05)
+        ev::KinematicEvents = events
+        ik = InterpKDE(bikernel);
+        p_is::Array{Float64,1} = map( (v,a) -> pdf(ik,v,a), ev.vels, ev.aecs);
+        try
+            p_kernel = kde(p_is,npoints = 2^13);
+            # Fix for numerical issues
+            p_kernel.density = p_kernel.density ./ (p_kernel.density |> sum)
+            # offset range due to weird left bound
+            offset_p = abs(p_kernel.x[1])
 
+            if(p_kernel.x[1] < 0)
+                p_kernel.x = p_kernel.x .+ offset_p;
+            end
+
+            # Get devient events by solving for the function
+            function GetStar()
+                for i in 1:size(p_kernel.density,1)
+                    # Solve for this
+                    if( (p_kernel.density[1:i] |> sum) > α )
+                        p_zero_star = p_kernel.x[(i-1)]
+                        return(p_zero_star);
+                    end
+                end
+                return nothing
+            end
+            p_zero_star = GetStar()
+
+            # get all p_is less than the zero_star
+            # offset p_is
+            p_is = p_is .+ offset_p
+            checks_p = p_is .< p_zero_star;
+            deviants = KinematicEvents(ev.vels[checks_p],ev.aecs[checks_p],ev.speedtype,ev.tstamps[checks_p])
+            de = new(α,events,bikernel,deviants,p_kernel,p_is,p_zero_star)
+            return(de)
+        catch e
+            print("ERROR: $e")
+            error("\n deviational events constructuion failed, try debugging, most likely KDE")
+        end
     end
 end
 
 
+function plot(dev::DeviationEvents)
+    vels::Array{Float64,1} = dev.deviants.vels;
+    aecs::Array{Float64,1} = dev.deviants.aecs;
+    scatter(vels,aecs, markersize = 2);
+end
 
+
+function plot!(dev::DeviationEvents)
+    vels::Array{Float64,1} = dev.deviants.vels;
+    aecs::Array{Float64,1} = dev.deviants.aecs;
+    scatter!(vels,aecs,markersize = 2);
+end
+
+
+#=
+
+abstract type TelemetricMap end;
+abstract type TelemetricEvent end;
+abstract type TelemetricEvents end;
+abstract type SpeedType end;
+
+ev = ke
+ik = InterpKDE(te.kernel)
+p_is = map( (v,a) -> pdf(ik,v,a), ev.vels, ev.aecs);
+p_kernel = kde(p_is);
+deviants = ev
+=#
 #=
 using Rmath,Plots
 
-a = rnorm(1000) |> x -> abs.(x)
-b = rnorm(1000)
-ke = KinematicEvents(a,b,KMH())
-te = KinematicMap(ke,[0.0,0.5],[-1.0,1.0])
+a = rnorm(10000) |> x -> abs.(x)
+b = rnorm(10000)
+ke = KinematicEvents(a,b,KMH(),repeat([DateTime(2019,1)],size(a,1)))
+te = KinematicMap(ke,[0.0,1],[-2.0,2.0])
+
+
+de = DeviationEvents(te.events,te.kernel,0.15)
+
+plot(te)
+scatter!(de.d)
+=#
+
+
+#=
+# Example with real data
+using CSV
+
+te = CSV.read("/Users/pocucan/Downloads/2011-08-25.csv")
+te = CSV.read("/Users/nik/Downloads/2011-08-25.csv")
+vels = te.speed_mph |> x -> convert(Array{Float64,1},x)
+aecs = te.accel_meters_ps |> x -> convert(Array{Float64,1},x)
+
+ke = KinematicEvents(vels,aecs,MPH(),repeat([DateTime(2019,1)],size(vels,1)))
+#mp = KinematicMap(ke,[30.0,50.0],[-5.0,5.0])
+mp = KinematicMap(ke,[40.0,90.0],[-2.0,2.0])
+
+dev = DeviationEvents(mp.events,mp.kernel,0.05)
+
+dev_1 = DeviationEvents(mp.events,mp.kernel,0.01)
+
 =#
--- src/TeleMap.jl
+++ src/TeleMap.jl
@@ -3,6 +3,7 @@ module TeleMap
 	abstract type TelemetricMap end;
 	abstract type TelemetricEvent end;
 	abstract type TelemetricEvents end;
+	abstract type SpeedType end;
 
 	include("Kinematic.jl")
 
